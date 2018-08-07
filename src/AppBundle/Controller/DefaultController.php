7f255fc4b979dc4b576af6b64203b914810f7aa1
13Hernaandeez <menailovethecrazyday@gmail.com>
2018-08-07 17:32:38 +0200

    nueva actualizacion

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,11 +1,10 @@
 <?php
 
 namespace AppBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
@@ -69,4 +68,25 @@ class DefaultController extends Controller
         $response->setContent($jsonContenido);
         return $response;
     }
-}
+
+          /**
+     * @Route("/noticia.{_format}/{id}", requirements={"id"="\d+"}, 
+     *          name="noticia_json_xml",
+     *          requirements={"_format": "json|xml"}
+     *              )
+     */
+          public function noticiaJsonXmlAction($id, $_format)
+          {
+            $repository = $this->getDoctrine()->getRepository('AppBundle:Noticia');
+            $noticia = $repository->findOneById($id);
+            $encoders = array(new XmlEncoder(), new JsonEncoder());
+            $normalizers = array(new ObjectNormalizer());
+            $serializer = new Serializer($normalizers, $encoders);
+            $Contenido = $serializer->serialize($noticia, $_format);
+            
+            $response = new Response();
+            $response->headers->set('Content-type', 'application/'.$_format);
+            $response->setContent($Contenido);
+            return $response;
+        }
+    }
